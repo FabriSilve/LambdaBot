221d39cac7b6eade6a161ebb3dacf819f2518b1b
Fabrizio Silvestri <fabrizio.silvestri@yahoo.it>
2019-12-08 18:46:28 +0100

    Send telegram request only on production

--- source/RequestManager.py
+++ source/RequestManager.py
@@ -2,6 +2,8 @@ import requests
 import os
 
 TOKEN = os.environ.get('TOKEN', 'token')
+ENV = os.environ.get('TOKEN', 'test')
+
 BASE_URL = "https://api.telegram.org/bot{}".format(TOKEN)
 
 class RequestManager:
@@ -20,7 +22,12 @@ class RequestManager:
 
         data = {"text": response.encode("utf8"), "chat_id": chat_id}
         url = BASE_URL + "/sendMessage"
-        requests.post(url, json=data)
+        if ENV == 'production':
+            requests.post(url, json=data)
+        else:
+            print('\nLOG:')
+            print(data)
+            print('')
 
 
 manager = RequestManager([])
