43f7e2b262c91eef24141fd2fb76d0f36f820ce3
Fabrizio Silvestri <fabrizio.silvestri@yahoo.it>
2019-12-08 18:27:18 +0100

    Add try catch to handle errors

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 
 app = Chalice(app_name='lambda-bot')
 
-TOKEN = os.environ['TOKEN']
+TOKEN = os.environ.get('TOKEN', 'token')
 BASE_URL = "https://api.telegram.org/bot{}".format(TOKEN)
 
 
@@ -12,17 +12,20 @@ BASE_URL = "https://api.telegram.org/bot{}".format(TOKEN)
 def index():
     body = app.current_request.json_body
 
-    message = str(body["message"]["text"])
-    chat_id = body["message"]["chat"]["id"]
-    first_name = body["message"]["chat"]["first_name"]
+    try:
+        message = str(body["message"]["text"])
+        chat_id = body["message"]["chat"]["id"]
+        first_name = body["message"]["chat"]["first_name"]
 
-    response = "Please /start, {}".format(first_name)
+        response = "Please /start, {}".format(first_name)
 
-    if "start" in message:
-        response = "Hello {}".format(first_name)
+        if "start" in message:
+            response = "Hello {}".format(first_name)
 
-    data = {"text": response.encode("utf8"), "chat_id": chat_id}
-    url = BASE_URL + "/sendMessage"
-    requests.post(url, json=data)
+        data = {"text": response.encode("utf8"), "chat_id": chat_id}
+        url = BASE_URL + "/sendMessage"
+        requests.post(url, json=data)
+    except:
+      print('Error handling telegram message')
 
     return {"statusCode": 200, "body": "ok"}
