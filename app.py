428a6225243fac234e13eefa3be0945b14e0db48
Fabrizio Silvestri <fabrizio.silvestri@yahoo.it>
2019-12-09 08:44:35 +0100

    Remove try catch in handler to see errors in logs

--- app.py
+++ app.py
@@ -7,10 +7,6 @@ app = Chalice(app_name='lambda-bot')
 
 @app.route('/', methods=['POST'])
 def index():
-    try:
-        body = app.current_request.json_body
-        manager.process(body)
-    except:  # noqa: E722
-        print('Error handling telegram message')
-    finally:
-        return {"statusCode": 200, "body": "ok"}
+    body = app.current_request.json_body
+    manager.process(body)
+    return {"statusCode": 200, "body": "ok"}
