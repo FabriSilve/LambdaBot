e1d0c54ee62e6fc5a2c3b330eddcfefa01572ad7
Fabrizio Silvestri <fabrizio.silvestri@yahoo.it>
2019-12-14 19:11:05 +0100

    Add test validation parameter answer

--- tests/Answer_test.py
+++ tests/Answer_test.py
@@ -11,3 +11,58 @@ class AnswerTest(TestCase):
 
         self.assertEqual(answer.answer_type, Answer.TEXT)
         self.assertEqual(answer.value, value)
+
+    def test_throw_error_if_type_not_allowed(self):
+        self.assertRaises(
+            AssertionError,
+            Answer,
+            'test',
+            'value',
+        )
+
+    def test_throw_error_if_type_not_defined(self):
+        self.assertRaises(
+            AssertionError,
+            Answer,
+            None,
+            'value',
+        )
+
+    def test_throw_error_if_type_not_string(self):
+        self.assertRaises(
+            AssertionError,
+            Answer,
+            33,
+            'value',
+        )
+
+    def test_throw_error_if_value_not_allowed(self):
+        self.assertRaises(
+            AssertionError,
+            Answer,
+            'test',
+            '',
+        )
+
+    def test_throw_error_if_value_not_defined(self):
+        self.assertRaises(
+            AssertionError,
+            Answer,
+            Answer.TEXT,
+            None,
+        )
+
+    def test_throw_error_if_value_parameter_is_missing(self):
+        self.assertRaises(
+            TypeError,
+            Answer,
+            Answer.TEXT,
+        )
+
+    def test_throw_error_if_value_not_string(self):
+        self.assertRaises(
+            AssertionError,
+            Answer,
+            Answer.TEXT,
+            33,
+        )
