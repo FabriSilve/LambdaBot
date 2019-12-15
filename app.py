0e313c2c187860fb87677734097dd3141f3293a6
Fabrizio Silvestri <fabrizio.silvestri@yahoo.it>
2019-12-15 17:31:24 +0100

    Pass if send or not http call from app

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from chalice import Chalice
+import os
 
 from chalicelib.process_request import process_request
 
 app = Chalice(app_name='lambda-bot')
 
+ENV = os.environ.get('ENV', 'test')
+
 
 @app.route('/', methods=['POST'])
 def index():
     body = app.current_request.json_body
-    return process_request(body)
+    can_send = ENV == 'production'
+    return process_request(body, can_send)
--- chalicelib/process_request.py
+++ chalicelib/process_request.py
@@ -2,12 +2,12 @@ import requests
 import os
 
 TOKEN = os.environ.get('TOKEN', 'token')
-ENV = os.environ.get('ENV', 'test')
+
 
 BASE_URL = "https://api.telegram.org/bot{}".format(TOKEN)
 
 
-def process_request(body):
+def process_request(body, can_send=True):
     message = str(body["message"]["text"])
     chat_id = body["message"]["chat"]["id"]
     first_name = body["message"]["chat"]["first_name"]
@@ -20,9 +20,9 @@ def process_request(body):
     data = {"text": response.encode("utf8"), "chat_id": chat_id}
     url = BASE_URL + "/sendMessage"
 
-    if ENV == 'local':
-        print('LOG:', data)
-    else:
+    if can_send:
         requests.post(url, json=data)
+    else:
+        print('LOG:', data)
 
     return {"statusCode": 200, "body": "ok"}
