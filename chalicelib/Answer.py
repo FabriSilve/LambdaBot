a6ad671d5db46e8a8ede2a1d88dee91e14c1fc66
Fabrizio Silvestri <fabrizio.silvestri@yahoo.it>
2019-12-15 11:41:22 +0100

    Remove assert checks adding more valid validation

--- chalicelib/Answer.py
+++ chalicelib/Answer.py
@@ -6,15 +6,19 @@ class Answer():
     IMAGE = 'image'
 
     def __init__(self, answer_type, value):
-        assert answer_type is not None
-        assert isinstance(answer_type, str)
-        assert answer_type == self.AUDIO \
-            or answer_type == self.TEXT \
-            or answer_type == self.IMAGE
+        if not answer_type:
+            raise AssertionError
+        if not isinstance(answer_type, str):
+            raise AssertionError
+        if answer_type not in [self.AUDIO, self.TEXT, self.IMAGE]:
+            raise AssertionError
 
-        assert value is not None
-        assert isinstance(value, str)
-        assert value.strip() != ''
+        if not value:
+            raise AssertionError
+        if not isinstance(value, str):
+            raise AssertionError
+        if not value.strip() != '':
+            raise AssertionError
 
         self.answer_type = answer_type
         self.value = value
