2e79e98e79529b0b7d3b1d6a66da6a5ea904be4b
Fabrizio Silvestri <fabrizio.silvestri@yahoo.it>
2019-12-16 19:16:19 +0100

    Souround everything with try

--- chalicelib/process_request.py
+++ chalicelib/process_request.py
@@ -9,24 +9,27 @@ BASE_URL = "https://api.telegram.org/bot{}".format(TOKEN)
 
 def process_request(body, can_send=True):
     print(body)
-    chat_id = body["message"]["chat"]["id"]
-    message = str(body["message"]["text"])
-    first_name = body["message"]["from"]["first_name"]
+    try:
+        chat_id = body["message"]["chat"]["id"]
+        message = str(body["message"]["text"])
+        first_name = body["message"]["from"]["first_name"]
 
-    response = "Please /start, {}".format(first_name)
+        response = "Please /start, {}".format(first_name)
 
-    if "start" in message:
-        response = "Hello {}".format(first_name)
+        if "start" in message:
+            response = "Hello {}".format(first_name)
 
-    data = {"text": response.encode("utf8"), "chat_id": chat_id}
-    url = BASE_URL + "/sendMessage"
+        data = {"text": response.encode("utf8"), "chat_id": chat_id}
+        url = BASE_URL + "/sendMessage"
 
-    if can_send:
-        try:
-            requests.post(url, json=data)
-        except:  # noqa: E722
-            print('Exception received', url, data)
-    else:
-        print('LOG:', data)
+        if can_send:
+            try:
+                requests.post(url, json=data)
+            except:  # noqa: E722
+                print('Exception received', url, data)
+        else:
+            print('LOG:', data)
+    except:  # noqa: E722
+        print('Exception received', body,)
 
     return {"statusCode": 200, "body": "ok"}
