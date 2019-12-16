96ef1ed713b4f84d215a30ac8e4e6c4e45a01930
Fabrizio Silvestri <fabrizio.silvestri@yahoo.it>
2019-12-16 17:07:09 +0100

    Revert chat id lock

--- chalicelib/process_request.py
+++ chalicelib/process_request.py
@@ -10,21 +10,20 @@ BASE_URL = "https://api.telegram.org/bot{}".format(TOKEN)
 def process_request(body, can_send=True):
     print(body)
     chat_id = body["message"]["chat"]["id"]
-    if chat_id == '244386202':
-        message = str(body["message"]["text"])
-        first_name = body["message"]["chat"]["first_name"]
+    message = str(body["message"]["text"])
+    first_name = body["message"]["chat"]["first_name"]
 
-        response = "Please /start, {}".format(first_name)
+    response = "Please /start, {}".format(first_name)
 
-        if "start" in message:
-            response = "Hello {}".format(first_name)
+    if "start" in message:
+        response = "Hello {}".format(first_name)
 
-        data = {"text": response, "chat_id": chat_id}
-        url = BASE_URL + "/sendMessage"
+    data = {"text": response, "chat_id": chat_id}
+    url = BASE_URL + "/sendMessage"
 
-        if can_send:
-            requests.post(url, json=data)
-        else:
-            print('LOG:', data)
+    if can_send:
+        requests.post(url, json=data)
+    else:
+        print('LOG:', data)
 
     return {"statusCode": 200, "body": "ok"}
